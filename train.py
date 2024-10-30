f2f3737bb932ad45f6c8a28fc23e260d90874bba
Alexandre Olech <alexandreolech7@gmail.com>
2024-10-30 21:44:29 +0100

    updated the files for latent_dim = 200

--- train.py
+++ train.py
@@ -3,59 +3,76 @@ import os
 from tqdm import trange
 import argparse
 from torchvision import datasets, transforms
-import torch.optim as optim
 import torch.nn as nn
-from model import Generator, Discriminator
-from utils import D_train_WGAN, G_train_WGAN, save_models
+import torch.optim as optim
 
+from model import Generator, Discriminator
+from utils import D_train, G_train, save_models
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train WGAN.')
-    parser.add_argument("--epochs", type=int, default=150, help="Number of epochs for training.")
-    parser.add_argument("--lr", type=float, default=0.01, help="The learning rate to use for training.")
-    parser.add_argument("--batch_size", type=int, default=64, help="Size of mini-batches for SGD")
-    parser.add_argument("--n_critic", type=int, default=5, help="Number of D updates per G update")
+    parser = argparse.ArgumentParser(description='Train Normalizing Flow.')
+    parser.add_argument("--epochs", type=int, default=100,
+                        help="Number of epochs for training.")
+    parser.add_argument("--lr", type=float, default=0.0002,
+                      help="The learning rate to use for training.")
+    parser.add_argument("--batch_size", type=int, default=64, 
+                        help="Size of mini-batches for SGD")
+    parser.add_argument("--latent_dim", type=int, default=100,
+                        help="Dimensionality of the latent space.")
 
     args = parser.parse_args()
+    latent_dim = args.latent_dim
 
     os.makedirs('checkpoints', exist_ok=True)
     os.makedirs('data', exist_ok=True)
 
+    # Data Pipeline
+    print('Dataset loading...')
+    # MNIST Dataset
     transform = transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize(mean=(0.5,), std=(0.5,))])
+                transforms.Normalize(mean=(0.5), std=(0.5))])
 
     train_dataset = datasets.MNIST(root='data/MNIST/', train=True, transform=transform, download=True)
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True)
+    test_dataset = datasets.MNIST(root='data/MNIST/', train=False, transform=transform, download=False)
+
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                               batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                              batch_size=args.batch_size, shuffle=False)
     print('Dataset Loaded.')
 
+
     print('Model Loading...')
-
-    # Model Initialization
     mnist_dim = 784
-    G = torch.nn.DataParallel(Generator(g_output_dim=mnist_dim)).cuda()
-    D = torch.nn.DataParallel(Discriminator(d_input_dim=mnist_dim)).cuda()
+    G = torch.nn.DataParallel(Generator(g_output_dim = mnist_dim, latent_dim=latent_dim)).cuda()
+    D = torch.nn.DataParallel(Discriminator(mnist_dim)).cuda()
+
+
+    # model = DataParallel(model).cuda()
     print('Model loaded.')
+    # Optimizer 
 
-    # Optimizers
-    G_optimizer = optim.RMSprop(G.parameters(), lr=args.lr)
-    D_optimizer = optim.RMSprop(D.parameters(), lr=args.lr)
+    # define loss
+    criterion = nn.BCELoss() 
 
-    # Training Loop
+    # define optimizers
+    G_optimizer = optim.Adam(G.parameters(), lr = args.lr)
+    D_optimizer = optim.Adam(D.parameters(), lr = args.lr)
+
+    print('Start Training :')
+    
     n_epoch = args.epochs
-    for epoch in trange(1, n_epoch + 1, leave=True):
+    for epoch in trange(1, n_epoch+1, leave=True):           
         for batch_idx, (x, _) in enumerate(train_loader):
-            x = x.view(-1, mnist_dim).cuda()
+            x = x.view(-1, mnist_dim)
+            D_train(x, G, D, D_optimizer, criterion, latent_dim=latent_dim)
+            G_train(x, G, D, G_optimizer, criterion, latent_dim=latent_dim)
 
-            # Train Discriminator (n_critic times per Generator update)
-            for _ in range(args.n_critic):
-                D_loss = D_train_WGAN(x, G,D.module, D_optimizer, clip_value=0.01)
-
-            # Train Generator
-            G_loss = G_train_WGAN(G, D.module, G_optimizer)  # Pass the current batch size
-
-        # Save model every 10 epochs
         if epoch % 10 == 0:
-            save_models(G, D, 'checkpoints')
-
+            save_models(G, D, f'checkpoints/latent_dim_{latent_dim}')
+                
     print('Training done')
+
+        
--- utils.py
+++ utils.py
@@ -1,126 +1,63 @@
-# import torch
-# import os
+import torch
+import os
+import mlflow
 
+def D_train(x, G, D, D_optimizer, criterion, latent_dim=100):
+    #=======================Train the discriminator=======================#
+    D.zero_grad()
 
+    # train discriminator on real
+    x_real, y_real = x, torch.ones(x.shape[0], 1)
+    x_real, y_real = x_real.cuda(), y_real.cuda()
 
-# def D_train(x, G, D, D_optimizer, criterion):
-#     #=======================Train the discriminator=======================#
-#     D.zero_grad()
+    D_output = D(x_real)
+    D_real_loss = criterion(D_output, y_real)
+    D_real_score = D_output
 
-#     # train discriminator on real
-#     x_real, y_real = x, torch.ones(x.shape[0], 1)
-#     x_real, y_real = x_real.cuda(), y_real.cuda()
+    # train discriminator on facke
+    z = torch.randn(x.shape[0], latent_dim).cuda()
+    x_fake, y_fake = G(z), torch.zeros(x.shape[0], 1).cuda()
 
-#     D_output = D(x_real)
-#     D_real_loss = criterion(D_output, y_real)
-#     D_real_score = D_output
-
-#     # train discriminator on facke
-#     z = torch.randn(x.shape[0], 100).cuda()
-#     x_fake, y_fake = G(z), torch.zeros(x.shape[0], 1).cuda()
-
-#     D_output =  D(x_fake)
+    D_output =  D(x_fake)
     
-#     D_fake_loss = criterion(D_output, y_fake)
-#     D_fake_score = D_output
+    D_fake_loss = criterion(D_output, y_fake)
+    D_fake_score = D_output
 
-#     # gradient backprop & optimize ONLY D's parameters
-#     D_loss = D_real_loss + D_fake_loss
-#     D_loss.backward()
-#     D_optimizer.step()
+    # gradient backprop & optimize ONLY D's parameters
+    D_loss = D_real_loss + D_fake_loss
+    D_loss.backward()
+    D_optimizer.step()
         
-#     return  D_loss.data.item()
+    return  D_loss.data.item()
 
+def G_train(x, G, D, G_optimizer, criterion, latent_dim=100):
+    #=======================Train the generator=======================#
+    G.zero_grad()
 
-# def G_train(x, G, D, G_optimizer, criterion):
-#     #=======================Train the generator=======================#
-#     G.zero_grad()
-
-#     z = torch.randn(x.shape[0], 100).cuda()
-#     y = torch.ones(x.shape[0], 1).cuda()
+    z = torch.randn(x.shape[0], latent_dim).cuda()
+    y = torch.ones(x.shape[0], 1).cuda()
                  
-#     G_output = G(z)
-#     D_output = D(G_output)
-#     G_loss = criterion(D_output, y)
+    G_output = G(z)
+    D_output = D(G_output)
+    G_loss = criterion(D_output, y)
 
-#     # gradient backprop & optimize ONLY G's parameters
-#     G_loss.backward()
-#     G_optimizer.step()
+    # gradient backprop & optimize ONLY G's parameters
+    G_loss.backward()
+    G_optimizer.step()
         
-#     return G_loss.data.item()
+    return G_loss.data.item()
 
-
-
-# def save_models(G, D, folder):
-#     torch.save(G.state_dict(), os.path.join(folder,'G.pth'))
-#     torch.save(D.state_dict(), os.path.join(folder,'D.pth'))
-
+def save_models(G, D, folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    torch.save(G.module.state_dict(), os.path.join(folder,'G.pth'))
+    torch.save(D.module.state_dict(), os.path.join(folder,'D.pth'))
 
 # def load_model(G, folder):
 #     ckpt = torch.load(os.path.join(folder,'G.pth'))
 #     G.load_state_dict({k.replace('module.', ''): v for k, v in ckpt.items()})
 #     return G
 
-import torch
-import os
-
-
-
-
-
-
-def D_train_WGAN(real_samples, G, D, optimizer, clip_value=0.01):
-    # Generate fake samples
-    z = torch.randn(real_samples.size(0), 100).cuda()
-    fake_samples = G(z)
-
-    # Calculate WGAN loss and gradient penalty
-    D_real = D(real_samples)
-    D_fake = D(fake_samples.detach())
-    
-    # Compute the gradient penalty
-    gradient_penalty = D.gradient_penalty(real_samples, fake_samples.detach())
-
-    # Combine losses
-    D_loss = D_fake.mean() - D_real.mean() + 5 * gradient_penalty
-    
-    optimizer.zero_grad()
-    D_loss.backward()
-    optimizer.step()
-
-    # Clip weights
-    for p in D.parameters():
-        p.data.clamp_(-clip_value, clip_value)
-
-    return D_loss.item()
-
-
-
-
-
-
-
-def G_train_WGAN(G, D, G_optimizer):
-    G.zero_grad()
-
-    # Generate fake images
-    z = torch.randn(64, 100).cuda()  # Use the batch size directly
-    x_fake = G(z)
-    D_fake = D(x_fake).mean()
-
-    # Generator loss (Wasserstein)
-    G_loss = -D_fake
-    G_loss.backward()
-    G_optimizer.step()
-
-    return G_loss.item()
-
-def save_models(G, D, folder):
-    torch.save(G.state_dict(), os.path.join(folder, 'G.pth'))
-    torch.save(D.state_dict(), os.path.join(folder, 'D.pth'))
-
-def load_model(G, folder):
-    ckpt = torch.load(os.path.join(folder, 'G.pth'))
-    G.load_state_dict({k.replace('module.', ''): v for k, v in ckpt.items()})
-    return G
-
+def load_model(model, folder):
+    model.load_state_dict(torch.load(os.path.join(folder, 'G.pth')))
+    return model
