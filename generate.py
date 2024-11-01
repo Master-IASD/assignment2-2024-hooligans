0fee66255740a07c9721fda77c0e10bb99e8547c
XCDU <lucas.rslt@gmail.com>
2024-11-01 23:16:15 +0100

    Gaussian Mixture generation

--- generate.py
+++ generate.py
@@ -1,45 +1,58 @@
-import torch 
+import torch
 import torchvision
 import os
-import shutil
 import argparse
+from GMmodel import Generator, GaussianM
+from GMutils import load_model
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-from model import Generator
-from utils import load_model
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate Normalizing Flow.")
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=2048,
+        help="The batch size to use for training.",
+    )
+    parser.add_argument("--model", type=str, default="checkpoints200")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate Normalizing Flow.')
-    parser.add_argument("--batch_size", type=int, default=2048,
-                      help="The batch size to use for training.")
-    parser.add_argument("--latent_dim", type=int, default=100,
-                        help="Dimensionality of the latent space.")
+    parser.add_argument("--d", type=int, default=100, help="latent space dimension")
+
     args = parser.parse_args()
-    #latent_dim = args.latent_dim
-    latent_dim = 200 # Hardcoded the latent_dim since the DsLAB platform runs a command without argument
-    print('Model Loading...')
+
+    print("Model Loading...")
     # Model Pipeline
     mnist_dim = 784
-    model = Generator(g_output_dim = mnist_dim, latent_dim=latent_dim).cuda()
-    model = load_model(model, f'checkpoints/latent_dim_{latent_dim}')
-    model = torch.nn.DataParallel(model).cuda()
-    model.eval()
+    K = 11
+    d = args.d
 
-    print('Model loaded.')
+    G = Generator(g_output_dim=mnist_dim, d=d).to(DEVICE)
+    GM = GaussianM(K, d).to(DEVICE)
+    load_model(G, GM, args.model)
+    G = torch.nn.DataParallel(G).to(DEVICE)
+    G.eval()
+    GM = torch.nn.DataParallel(GM).to(DEVICE)
+    GM.eval()
 
-    print('Start Generating')
-    # Clean up the 'samples' directory if it exists
-    if os.path.exists('samples'):
-        shutil.rmtree('samples')
-    os.makedirs('samples', exist_ok=True)
+    print("Model loaded.")
+
+    print("Start Generating")
+    os.makedirs("samples", exist_ok=True)
 
     n_samples = 0
     with torch.no_grad():
-        while n_samples<10000:
-            z = torch.randn(args.batch_size, latent_dim).cuda()
-            x = model(z)
+        while n_samples < 10000:
+            k_values = torch.randint(0, 10, (args.batch_size,))
+            y = torch.eye(K)[k_values].to(DEVICE)
+            N = torch.distributions.MultivariateNormal(torch.zeros(d), torch.eye(d))
+            z = N.sample((args.batch_size,)).to(DEVICE).to(torch.float32)
+            z_tilde = GM(y, z)
+            x = G(z_tilde)
             x = x.reshape(args.batch_size, 28, 28)
             for k in range(x.shape[0]):
-                if n_samples<10000:
-                    torchvision.utils.save_image(x[k:k+1], os.path.join('samples', f'{n_samples}.png'))         
+                if n_samples < 10000:
+                    torchvision.utils.save_image(
+                        x[k : k + 1], os.path.join("samples", f"{n_samples}.png")
+                    )
                     n_samples += 1
